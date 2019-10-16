3a8244719c015eebe74470231573f8e949f8e93e
sambc <sambcollins@gmail.com>
2019-10-15 21:38:29 -0400

    player movement

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ var c = canvas.getContext('2d');
 var height = canvas.height
 var width = canvas.width
 var image = document.createElement('img')
-var unit = width/20
+var widthunit = width/20
+var heightunit = height/20
 var characters = [  
     player = {
         image: 'player',
-        y: height-unit*3,
-        x: unit*6,
-        height: unit*2,
-        width: unit*2
+        y: height-heightunit*3,
+        x: widthunit*6,
+        height: heightunit*2,
+        width: widthunit*2
     }
 ]
-function reDrawCanvas () {
+function upDate() {
     c.fillStyle = 'blue'
     c.fillRect(0,0,width,height/2)
     c.fillStyle = 'green'
@@ -21,9 +22,24 @@ function reDrawCanvas () {
     for (i=0; i < characters.length; i++) {
         var image = document.getElementById(characters[i].image)
         c.drawImage(image, characters[i].x, characters[i].y,characters[i].width, characters[i].height)
-        console.log(characters[i])
     }   
+    requestAnimationFrame(upDate)
 }
-requestAnimationFrame(reDrawCanvas)
 
-// reDrawCanvas()
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode == '38'){
+        characters[0].y -= heightunit;
+    }
+    if (event.keyCode == '40') {
+        characters[0].y += heightunit;
+    }
+    if (event.keyCode == '37'){
+        characters[0].x -= heightunit;
+    }
+    if (event.keyCode == '39') {
+        characters[0].x += heightunit;
+    }
+})
+
+
+requestAnimationFrame(upDate)
