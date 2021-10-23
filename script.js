0dcba354c3d8c297a84c4e5476cb3e60bdd970e5
Nathaniel Collins <nathanielcollins@Nathaniels-MacBook-Pro.local>
2021-10-23 13:56:12 -0400

    initial commits

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-var canvas = document.getElementById('canvas');
-var c = canvas.getContext('2d');
-var height = canvas.height
-var width = canvas.width
-var image = document.createElement('img')
-var widthunit = width/20
-var heightunit = height/20
-var characters = [  
-    player = {
-        image: 'player',
-        y: height-heightunit*3,
-        x: widthunit*6,
-        height: heightunit*2,
-        width: widthunit*2
-    }
-]
-function upDate() {
-    c.fillStyle = 'blue'
-    c.fillRect(0,0,width,height/2)
-    c.fillStyle = 'green'
-    c.fillRect(0,height/2,width,height/2)
-    for (i=0; i < characters.length; i++) {
-        var image = document.getElementById(characters[i].image)
-        c.drawImage(image, characters[i].x, characters[i].y,characters[i].width, characters[i].height)
-    }   
-    requestAnimationFrame(upDate)
+var canvas = document.getElementById("canvas");
+
+var c = canvas.getContext("2d");
+
+
+points = []
+
+function makeArt(){
+    c.fillStyle = 'rgb(236, 235, 229)'
+    c.fillRect(0,0,900, 500)
+    
+    createPoints()
+    console.log(points)
+    createLines()
 }
+function createPoints(){
+    points = []
+    for (x = 0; x < 13; x++){
+        points.push([Math.random()*canvas.width, Math.random()*canvas.height])
+    }
+}
+function createLines(){
+c.beginPath()
+c.moveTo(points[0][0], points[0][1])
+for (x = 1; x < points.length; x++){
+c.lineTo(points[x][0], points[x][1])
+}
+c.stroke()
 
-document.addEventListener("keydown", function(event) {
-    if (event.keyCode == '38'){
-        characters[0].y -= heightunit;
-    }
-    if (event.keyCode == '40') {
-        characters[0].y += heightunit;
-    }
-    if (event.keyCode == '37'){
-        characters[0].x -= heightunit;
-    }
-    if (event.keyCode == '39') {
-        characters[0].x += heightunit;
-    }
-})
-
-
-requestAnimationFrame(upDate)
+}
